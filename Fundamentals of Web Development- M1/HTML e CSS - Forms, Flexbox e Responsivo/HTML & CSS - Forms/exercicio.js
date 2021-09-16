6ae4296badf1b7b48d715b8f40169043e32d9b7d
Bruno Alves Fay <brunofay1@hotmail.com>
2021-09-15 23:18:31 -0300

    Block 6 part of exercicies

--- Fundamentals of Web Development- M1/HTML e CSS - Forms, Flexbox e Responsivo/HTML & CSS - Forms/exercicio.js
+++ Fundamentals of Web Development- M1/HTML e CSS - Forms, Flexbox e Responsivo/HTML & CSS - Forms/exercicio.js
@@ -1,4 +1,4 @@
-let stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+let stateOptions = ['Selecione um Estado', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
 const getEstados = document.querySelector('#estados')
 function estado() {
     for (let index = 0; index < stateOptions.length; index += 1) {
@@ -16,12 +16,27 @@ if (catchElement.value.length > tamanhoMaximo ||catchElement.value.length < 0){
     alert(`verifique o ${inputName}`)
 }
 }
-verificName('name',40)
+function verificNumbers(inputName) {
+    let catchElement = document.querySelector(`[name = ${inputName}]`) 
+   /* expressão para verificar se existe somente um numero no valor do input https://pt.stackoverflow.com/questions/11275/verificar-se-string-possui-apenas-n%C3%BAmeros */
+    if (isNaN(parseFloat(catchElement.value))&& !isFinite(catchElement.value)){
+        alert(`verifique o ${inputName}`)
+
+    }
+    
+}
+
 
 window.onload= () =>{
     const getButton = document.querySelector('#curriculo')
     getButton.addEventListener('click',(event) =>{
         event.preventDefault()
+        verificNumbers()
+        verificNumbers('cpf')
+        verificName('name',40)
+        verificName('endereço',200)
+        verificName('cidade',28)
+        verificName('email',50)
 
     })
 }
