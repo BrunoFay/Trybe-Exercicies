0ebf99b7c617133ca33904ece8c2368cecf5f7a3
Bruno Alves Fay <brunofay1@hotmail.com>
2021-09-17 15:27:21 -0300

    exercicie 6.2

---
+++ Fundamentals of Web Development- M1/HTML e CSS - Forms, Flexbox e Responsivo/Bibliotecas JavaScript e Frameworks CSS/script.js
@@ -0,0 +1,5 @@
+import './node_modules/just-validate/dist/js/just-validate.js'
+window.onload = () => {
+new window.JustValidate('.js-form');
+}
+/* ESta funcionando, só implementar as regras e as mensagens  */
