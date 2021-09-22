6401de5f61423df6fcb1a2f857479d89bd351cdf
Bruno Alves Fay <brunofay1@hotmail.com>
2021-09-21 23:14:29 -0300

    desktop and ipad

--- Fundamentals of Web Development- M1/HTML e CSS - Forms, Flexbox e Responsivo/HTML & CSS - Forms/exercicio.js
+++ Fundamentals of Web Development- M1/HTML e CSS - Forms, Flexbox e Responsivo/HTML & CSS - Forms/exercicio.js
@@ -16,27 +16,42 @@ function verificName(inputName, tamanhoMaximo) {
         alert(`verifique o ${inputName}`)
     }
 }
-function verificNumbers(inputName) {
+function verificNumbers(inputName,tamanhoMaximo) {
     let catchElement = document.querySelector(`[name = ${inputName}]`)
     /* expressão para verificar se existe somente um numero no valor do input https://pt.stackoverflow.com/questions/11275/verificar-se-string-possui-apenas-n%C3%BAmeros */
     if (isNaN(parseFloat(catchElement.value)) && !isFinite(catchElement.value)) {
         alert(`verifique o ${inputName}`)
 
     }
+    else if(catchElement.value.length > tamanhoMaximo || catchElement.value.length < 0){
+        alert(`verifique o ${inputName}, tamanho estendido`)
+    }
 
 }
-
-
+let curriculo = {}
+function objetos(inputId) {
+    let catchElement = document.getElementById(inputId).value
+    curriculo[`${inputId}`] = catchElement
+}
+objetos('name')
+objetos('cidade')
 window.onload = () => {
-    const getButton = document.querySelector('#curriculo')
-    getButton.addEventListener('click', (event) => {
+    const getButtonCreat = document.querySelector('#curriculo')
+    const getButtonReset = document.querySelector('#reset')
+    verificNumbers('cpf',11)
+    verificName('name', 40)
+    verificName('endereço', 200)
+    verificName('cidade', 28)
+    verificName('email', 50)
+    verificName('descricao-de-carg', 500)
+    verificName('cargos', 40)
+    verificName('resume', 1000)
+    getButtonReset.addEventListener('click',(event)=>{
+        
+    })
+    getButtonCreat.addEventListener('click', (event) => {
         event.preventDefault()
-        verificNumbers()
-        verificNumbers('cpf')
-        verificName('name', 40)
-        verificName('endereço', 200)
-        verificName('cidade', 28)
-        verificName('email', 50)
-
+        console.log(curriculo);
+    
     })
 }
