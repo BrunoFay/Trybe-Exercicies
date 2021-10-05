46b1491b065fce1b80483f42c87383c8b7aa289e
Bruno Alves Fay <brunofay1@hotmail.com>
2021-10-05 20:19:12 -0300

    8.2 lets pratice

---
+++ Fundamentals of Web Development- M1/B8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort/vamos-praticar-some-every.js
@@ -0,0 +1,55 @@
+/* const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+
+console.log(verifyFirstLetter('J', listNames)); // true
+console.log(verifyFirstLetter('x', listNames)); // false */
+
+
+// ex2
+
+/* const grades = {
+    portugues: 'Aprovado',
+    matematica: 'Reprovado',
+    ingles: 'Aprovado',
+  };
+  
+  const verifyGrades = (studentGrades) => (
+    Object.values(studentGrades).every((grade) => grade === 'Aprovado')
+  );
+  
+  console.log(verifyGrades(grades)); */
+
+
+  //ex3
+
+
+
+
+/*   const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => { return arr.some((nome)=> name === nome) 
+  //Adicione seu código aqui
+}
+
+console.log(hasName(names, 'Mateus')) */
+
+
+
+//ex4
+
+
+/* const peoples = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  const verifyAges = (arr, minimumAge) => (arr).every((people)=> people.age >= minimumAge )
+    //Adicione seu código aqui
+  
+  
+  console.log(verifyAges(peoples, 18));
+ */
