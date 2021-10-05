0261c4c884b104928d26086c4928c85301b568e6
Bruno Alves Fay <brunofay1@hotmail.com>
2021-10-05 15:04:26 -0300

    bonus part 1 70%

--- Fundamentals of Web Development- M1/B8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Introdução a Higher Order Functions/exercicies/bonus.js
+++ Fundamentals of Web Development- M1/B8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Introdução a Higher Order Functions/exercicies/bonus.js
@@ -1,45 +1,54 @@
-const dragonDamage = (dragon) => {
+/* const dragonDamage = (dragon) => {
   const damage = Math.floor(Math.random() * (dragon['strength'] - 15))
   return damage
-};
+}; */
 
-const warriorDamage = (warrior) => {
-  const damage = Math.floor(Math.random() * ((warrior['strength'] * warrior['weaponDmg']) - warrior['strength']))
+
+/* 
+const mageDamage = mage => {
+  const damage = Math.floor(Math.random() *
+    ((mage['intelligence'] * 2) - mage['intelligence']))
   return damage
-};
-const mageDamage = (mage) => { const damage = Math.floor(Math.random() * ((mage['intelligence'] * 2) - mage['intelligence'])) 
-return damage};
-const mana =() => (mage.mana < 15)? console.log('nao possui mana suficiente'): mage.mana - 15;
+}; */
 
-const magiaGastaPorAtaque = (mage) => { ({ dano: mageDamage, manaGasta: mana }) }
+/* const mana = () => (mage['mana'] < 15) ? console.log('nao possui mana suficiente') : mage['mana'] - 15;
+
+const magiaGastaPorAtaque = (mage) => { ({ dano: mageDamage, manaGasta: mana }) } */
+
 
 
 const mage = {
   healthPoints: 130,
   intelligence: 45,
   mana: 125,
-  damage: magiaGastaPorAtaque,
+  damage: () => { ({ dano: () =>  Math.floor(Math.random() *
+    ((mage['intelligence'] * 2) - mage['intelligence'])), 
+    manaGasta: () => (mage['mana'] < 15) ? console.log('nao possui mana suficiente') : mage['mana'] - 15 }) },
 };
 
 const warrior = {
   healthPoints: 200,
   strength: 30,
   weaponDmg: 2,
-  damage: warriorDamage,
+  damage: () => Math.floor(Math.random() * ((warrior.strength * warrior.weaponDmg) - warrior.strength))
 };
 
 const dragon = {
   healthPoints: 350,
   strength: 50,
-  damage: dragonDamage()
+  damage:() => Math.floor(Math.random() * (dragon['strength'] - 15))
 };
 
 const battleMembers = { mage, warrior, dragon };
 
 
-console.log(dragon.damage.dragonDamage);
+
+
+
+const damage = () => Math.floor(Math.random() * ((warrior.strength * warrior.weaponDmg) - warrior.strength));
+console.log(dragon.damage());
 
 /* const gameActions = {
   // Crie as HOFs neste objeto.
-  const warriorAtackDragon = () => 
+  const warriorAtackDragon = () =>
 }; */
