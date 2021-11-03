c6afe967d5b4ca96075eeff0d129d79155c22aa5
Bruno Alves Fay <brunofay1@hotmail.com>
2021-11-03 16:04:11 -0300

    vamos praticar feitos

--- Desenvolvimento Front-end/B11 - Componentes com Estado, Eventos e Formulários com React/intro events/fancy-buttons/src/component/buttons.jsx
+++ Desenvolvimento Front-end/B11 - Componentes com Estado, Eventos e Formulários com React/intro events/fancy-buttons/src/component/buttons.jsx
@@ -4,30 +4,44 @@ import React from 'react';
 
 
 class App extends React.Component {
-    constructor(){
+    constructor() {
         super()
         this.handleButtonOne = this.handleButtonOne.bind(this);
         this.handleButtonTwo = this.handleButtonTwo.bind(this);
         this.handleButtonThree = this.handleButtonThree.bind(this)
+        this.state = {
+            numeroDeCliquesB1: 0,
+            numeroDeCliquesB2: 0,
+            numeroDeCliquesB3: 0,
+        }
     }
     handleButtonOne() {
-        console.log('Clicou no botão 1!');
-        console.log(this);
+        this.setState((lastState, _props) => ({
+            numeroDeCliquesB1: lastState.numeroDeCliquesB1 + 1
+        }))
     }
 
     handleButtonTwo() {
-        console.log('Clicou no botão 2!');
+        this.setState((lastState, _props) => ({
+            numeroDeCliquesB2: lastState.numeroDeCliquesB2 + 1
+        }))
     }
 
     handleButtonThree() {
-        console.log('Clicou no botão 3!');
+        this.setState((lastState, _props) => ({
+            numeroDeCliquesB3: lastState.numeroDeCliquesB3 + 1
+        }))
+    }
+    evenOurOdd(num) {
+        return (num % 2 === 0) ? 'yellow' : 'red'
     }
     render() {
+        const {numeroDeCliquesB1,numeroDeCliquesB2,numeroDeCliquesB3} = this.state; 
         return (
             <div>
-                <button onClick={this.handleButtonOne}>Botão 1</button>
-                <button onClick={this.handleButtonTwo}>Botão 2</button>
-                <button onClick={this.handleButtonThree}>Botão 3</button>
+                <button onClick={this.handleButtonOne} style={{ backgroundColor: this.evenOurOdd(numeroDeCliquesB1) }}>{numeroDeCliquesB1}</button>
+                <button onClick={this.handleButtonTwo} style={{ backgroundColor: this.evenOurOdd(numeroDeCliquesB2) }}>{numeroDeCliquesB2}</button>
+                <button onClick={this.handleButtonThree} style={{ backgroundColor: this.evenOurOdd(numeroDeCliquesB3) }}>{numeroDeCliquesB3}</button>
             </div>
         );
     }
