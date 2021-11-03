0e9a379ab7e9fec8e34179603e82944b1342967f
Bruno Alves Fay <brunofay1@hotmail.com>
2021-11-03 15:18:23 -0300

    block 11.1

---
+++ Desenvolvimento Front-end/B11 - Componentes com Estado, Eventos e Formulários com React/intro events/fancy-buttons/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Button from './component/buttons';
+
+function App() {
+  return (
+    <div className="App">
+     <Button />
+    </div>
+  );
+}
+
+export default App;
