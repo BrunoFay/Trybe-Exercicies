f06c0ba8ce868e6cbb318e0a3428f6970812326e
Bruno Alves Fay <brunofay1@hotmail.com>
2022-03-11 17:43:28 -0300

    1 done

---
+++ Desenvolvimento Back-end/B22,23,24 - NodeJs/22.1 Um motor JavaScript/exercicies/index.js
@@ -0,0 +1,11 @@
+const { question, questionFloat,questionInt } = require('readline-sync');
+
+const name = question('Qual seu nome? ');
+
+const altura = questionFloat('Qual sua altura?');
+
+const peso = questionInt('Qual seu peso? ');
+const result = peso/( Math.pow(altura, 2))
+
+
+console.log(`Hello, ${name}! You IMC is ${result}!`);
