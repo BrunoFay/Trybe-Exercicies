8b776ff0bd62c8112a4e6ce0cb3e6d228df10963
Bruno Alves Fay <brunofay1@hotmail.com>
2022-03-12 19:51:23 -0300

    1 done

---
+++ Desenvolvimento Back-end/B22,23,24 - NodeJs/22.2 Fluxo Assíncrono/exercicies/exe1.js
@@ -0,0 +1,22 @@
+async function mathForm(one, two, three) {
+  if (
+    typeof one !== "number" ||
+    typeof two !== "number" ||
+    typeof three !== "number") throw new Error("Informe apenas números")
+  const result = (one + two) * three
+  if (result < 50) throw new Error("Valor muito baixo")
+  else {
+    console.log(result);
+  }
+}
+async function main() {
+  try {
+    await mathForm(1, 2, 5)
+
+  }
+  catch (error) {
+    console.log(error.message);
+  }
+
+}
+main()
