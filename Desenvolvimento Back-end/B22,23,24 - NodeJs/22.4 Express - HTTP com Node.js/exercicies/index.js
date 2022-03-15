0ea28218fb908b158604469b84ea29cca0f19822
Bruno Alves Fay <brunofay1@hotmail.com>
2022-03-15 18:55:14 -0300

    6 a 8 done

--- Desenvolvimento Back-end/B22,23,24 - NodeJs/22.4 Express - HTTP com Node.js/exercicies/index.js
+++ Desenvolvimento Back-end/B22,23,24 - NodeJs/22.4 Express - HTTP com Node.js/exercicies/index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser');
 const fs = require('fs').promises
 
+const { readSimpsons, writeSimpsons } = require('./controllersFun.js')
 app.use(bodyParser.json())
 
 app.listen(3003, () => {
@@ -34,5 +35,23 @@ app.get('/simpsons', async (req, res) => {
   const simpsonsFile = await fs.readFile('./simpsons.json', 'utf-8')
     .then(fileContent => JSON.parse(fileContent));
   if (simpsonsFile) return res.status(200).json(simpsonsFile)
-  return res.status(500)
+  return res.status(500).end()
+})
+
+app.get('/simpsons/:id', async (req, res) => {
+  const { id } = req.params
+  const simpsons = await readSimpsons()
+  const filterSimpson = simpsons.find((simp) => simp.id === id)
+  if (filterSimpson) return res.status(202).json(filterSimpson)
+
+  res.status(404).json({ message: 'simpson not found' })
+})
+
+app.post('/simpsons', async(req, res) => {
+  const { id, name } = req.body
+  const simpsons = await readSimpsons()
+  const checkIdInSimpsonsFile = simpsons.some((simp) => simp.id === id)
+  if (checkIdInSimpsonsFile) return res.status(409).json({ message: 'id already exists' })
+  writeSimpsons([...simpsons,{ id, name }])
+  res.status(204).end()
 })
