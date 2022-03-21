9082e70c104535de933c39e0d588738e3cc8441b
Bruno Alves Fay <brunofay1@hotmail.com>
2022-03-21 18:45:45 -0300

    put routes implemented

--- Desenvolvimento Back-end/B22,23,24 - NodeJs/23.1 Arquitetura de Software - Camada de Model/let's pratice/model-example/index.js
+++ Desenvolvimento Back-end/B22,23,24 - NodeJs/23.1 Arquitetura de Software - Camada de Model/let's pratice/model-example/index.js
@@ -2,10 +2,9 @@
 
 const express = require('express');
 const Author = require('./models/Author');
-const Book =require('./models/Book');
-const { query } = require('./models/connection');
+const Book = require('./models/Book');
 const app = express();
-
+app.use(express.json())
 const PORT = process.env.PORT || 3000;
 
 app.get('/authors', async (_req, res) => {
@@ -25,10 +24,32 @@ app.get('/authors/:id', async (req, res) => {
 });
 
 app.get('/books/:author_id', async (req, res) => {
-  const {author_id} = req.params
+  const { author_id } = req.params
   const books = await Book.getByAuthorId(author_id);
-  if(books.length === 0)return res.status(404).json({message:'not found'})
+  if (books.length === 0) return res.status(404).json({ message: 'not found' })
   res.status(200).json(books);
+});
+
+app.get('/books/', async (_req, res) => {
+  const books = await Book.getAll();
+  res.status(200).json(books);
+});
+app.post('/books', async (req, res) => {
+  const { title, author_id } = req.body
+  if (!Book.validatePostNewBook) return res.status(400).json({ message: 'dados invalidos' })
+  await Book.createNewBook(title, author_id)
+  res.status(200).json({ message: 'livro adicionado' })
+})
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+    return res.status(400).json({ message: 'Dados inválidos' });
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'Autor criado com sucesso! ' });
 });
 
 app.listen(PORT, () => {
--- Desenvolvimento Back-end/B22,23,24 - NodeJs/23.1 Arquitetura de Software - Camada de Model/let's pratice/model-example/models/Book.js
+++ Desenvolvimento Back-end/B22,23,24 - NodeJs/23.1 Arquitetura de Software - Camada de Model/let's pratice/model-example/models/Book.js
@@ -1,13 +1,13 @@
 const connection = require('./connection')
 
-async function getAll(){
-const [books]= await connection.execute(
-  'SELECT  * FROM model_example.books;',
-)
-return books
+async function getAll() {
+  const [books] = await connection.execute(
+    'SELECT  * FROM model_example.books;',
+  )
+  return books
 }
-async function getByAuthorId(athId){
-  const [books]= await connection.execute(
+async function getByAuthorId(athId) {
+  const [books] = await connection.execute(
     'SELECT * FROM model_example.books WHERE author_id=?;',
     [athId]
   )
@@ -15,7 +15,24 @@ async function getByAuthorId(athId){
   return books
 }
 
-module.exports={
+async function validatePostNewBook(title, author_id) {
+  const [authors] = await connection.execute(
+    'SELECT * FROM model_example.authors WHERE author_id=?;',
+    [author_id]
+  )
+
+  if (!title || title.length < 3) return false
+  else if (!author_id || authors.length === 0) return false
+}
+async function createNewBook(title, author_id) {
+  connection.execute(
+    'INSERT INTO model_example.books (title,author_id) VALUES (?,?)',
+    [title, author_id]
+  )
+}
+module.exports = {
   getAll,
-  getByAuthorId
+  getByAuthorId,
+  validatePostNewBook,
+  createNewBook
 }
