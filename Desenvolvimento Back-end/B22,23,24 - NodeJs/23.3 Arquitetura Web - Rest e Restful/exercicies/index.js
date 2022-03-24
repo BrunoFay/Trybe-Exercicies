51d89f9fa398d6f92e40f1bc804e0e50f0883226
Bruno Alves Fay <brunofay1@hotmail.com>
2022-03-24 15:39:09 -0300

    setup for exercicies

---
+++ Desenvolvimento Back-end/B22,23,24 - NodeJs/23.3 Arquitetura Web - Rest e Restful/exercicies/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const productRoute =require('./controllers/productController')
+requeri('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/products', productRoute);
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000!");
+});
