65defffa623d11e5c571555dd61a9bbe913377cd
BRUNO <brunofay1@hotmail.com>
2022-04-14 14:30:44 -0300

    b25 heroku

---
+++ Desenvolvimento Back-end/B25 - Heroku/exercicios/api-to-delploy-heroku/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const app = express()
+const PORT = process.env.PORT || 3030
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  return res.status(200).json({ message: 'esta vivo' })
+})
+app.listen(PORT, () => { console.log(`server online in port ${PORT}`); })
