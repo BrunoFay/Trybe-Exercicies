0323c3e99f7b5c014f6e9a36d4f629657dfe9d9e
BRUNO <brunofay1@hotmail.com>
2022-04-14 15:03:52 -0300

    TEST mundando a mensagem de resposta

--- Desenvolvimento Back-end/B25 - Heroku/exercicios/api-to-delploy-heroku/index.js
+++ Desenvolvimento Back-end/B25 - Heroku/exercicios/api-to-delploy-heroku/index.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 3030
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  return res.status(200).json({ message: 'esta vivo' })
+  return res.status(200).json({ message: 'esta vivo!' })
 })
 app.listen(PORT, () => { console.log(`server online in port ${PORT}`); })
